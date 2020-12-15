d347d1c83628cdc4c1b1d32302f7e51369801e2c
Joell Jones <25757151+joelljones@users.noreply.github.com>
2020-12-15 16:19:59 -0500

    cra, material-ui, skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import { ... } from '@material-ui/icons';
+import 'fontsource-roboto';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
